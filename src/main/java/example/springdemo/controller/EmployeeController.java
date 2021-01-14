f8cc935d46493b99eeabeb1991cd2d13cf7a86ed
hrithik-hs <hrithik237@gmail.com>
2021-01-14 22:44:20 +0530

    initial commit

---
+++ src/main/java/example/springdemo/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package example.springdemo.controller;
+
+import java.util.List;
+
+import example.springdemo.model.Employee;
+import example.springdemo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+}
